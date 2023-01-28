24a55a98d3c96ef7bfaffb9318e3157f2cf2e4ca
p-mcgowan <mcgowanapat+git@gmail.com>
2023-01-28 19:38:53 +0100

    stage1

---
+++ readme.md
@@ -0,0 +1,8 @@
+# fst demo
+
+
+## stage1
+basic docker compose plugin
+
+### step1
+scaffold services
--- templates/compose/fstr/compose.js
+++ templates/compose/fstr/compose.js
@@ -1,121 +1,87 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.render = void 0;
 const fs_1 = require("fs");
-const service = () => {
-    [];
+const path_1 = require("path");
+const baseDocker = (name, path) => {
+    return [
+        `  ${name}:`,
+        `    container_name: ${name}`,
+        '    build:',
+        `      context: ${path}`,
+        '      network: host',
+        '    networks:',
+        '      - backend-network',
+        '    env_file:',
+        '      - ./.env',
+    ];
 };
-`main-backend:
-    container_name: main-backend
-    build:
-      network: host
-      context: ../main-backend
-    env_file:
-      - ../main-backend/.env.example
-      - ./.env
-    environment:
-      - PORT=${'MAIN_BACKEND_PORT'}
-      - PGHOST=pg.test
-      - NODE_ENV=production
-    ports:
-      - "5000:5000"
-    depends_on:
-      - db
-    networks:
-      - backend-network
-    volumes:
-      - ../main-backend/server.ts:/code/server.ts
-      - ../main-backend/tsconfig.json:/code/tsconfig.json
-      - ../main-backend/tsconfig.prod.json:/code/tsconfig.prod.json
-      - ../main-backend/src:/code/src/
-      - ../main-backend/openapi-nodegen-api-file.yml:/code/openapi-nodegen-api-file.yml
-    working_dir: /code
-    command: watch
-
-  db:
-    container_name: db
-    image: postgres:13.6
-    restart: always
-    env_file:
-      - ./.env
-    environment:
-      - PGHOST=pg.test
-      - POSTGRES_PASSWORD=postgres
-    ports:
-      - "5432:5432"
-    networks:
-      backend-network:
-        aliases:
-          - pg.test
-    volumes:
-      - ./volumes/pg:/var/lib/postgresql/data
-    deploy:
-      resources:
-        limits:
-          cpus: '1'
-          memory: 1g
-    logging:
-      driver: none
-
-  parts-api:
-    container_name: parts-api
-    build:
-      context: ./parts-api/
-      network: host
-    networks:
-      - backend-network
-    environment:
-      - PORT=9999
-      - BASE_URL=http://localhost:9999
-    init: true
-    ports:
-      - "9999:9999"
-
-  service-case-backend:
-    depends_on:
-      - java-common
-      - db
-    container_name: service-case-backend
-    build:
-      network: host
-      context: ../service-case-backend
-      args:
-        ADDITIONAL_GRADLE_PARAMS: -x test
-    environment:
-      - PORT=${'SERVICE_CASE_BACKEND_PORT'}
-      - INITIAL_RAM_PERCENTAGE=5
-      - MAX_RAM_PERCENTAGE=20
-    networks:
-      - backend-network
-    ports:
-      - "5003:8080"
-    env_file:
-      - .env.example
-      - .env
-
-networks:
-  ${'project'}-network:
-    name: ${'project'}-network
-`;
-const services = (recipe) => {
-    for (const service of recipe.data.services) {
-        console.log(recipe.map[service]);
+const getServiceForType = {
+    postgres: (name, path) => {
+        return [
+            `  ${name}:`,
+            `    container_name: ${name}`,
+            '    image: postgres:13.6',
+            '    restart: always',
+            '    env_file:',
+            '      - ./.env',
+            '    environment:',
+            '      - POSTGRES_PASSWORD=postgres',
+            '    ports:',
+            '      - "5432:5432"',
+            '    networks:',
+            '      - backend-network',
+            '    volumes:',
+            '      - ./volumes/pg:/var/lib/postgresql/data',
+        ].join('\n');
+    },
+    node: (name, path, type) => {
+        return baseDocker(name, path).concat(['    environment:', '      NODE_ENV: "production"']).join('\n');
+    },
+    java: (name, path, type) => {
+        return baseDocker(name, path).concat(['    environment:', '      MEM_USAGE: "600TB"']).join('\n');
+    },
+};
+const detectTemplateType = async (path) => {
+    const files = await fs_1.promises.readdir(path);
+    for (const file of files) {
+        if (file === 'package.json') {
+            return 'node';
+        }
+        if (file === 'build.gradle') {
+            return 'java';
+        }
     }
-    return `\
-services:
-  abcd:
-    container_name: "yolo"
-    image: nginx`;
+    return 'default';
+};
+const getServiceBuilder = (recipe) => {
+    return async () => {
+        const services = [];
+        for (const [name, service] of Object.entries(recipe.data.services)) {
+            const serviceRecipe = recipe.map[name];
+            const templateType = service.type || (await detectTemplateType(serviceRecipe.to));
+            services.push(getServiceForType[templateType]((service === null || service === void 0 ? void 0 : service.name) || name, (serviceRecipe === null || serviceRecipe === void 0 ? void 0 : serviceRecipe.to) ? (0, path_1.relative)(recipe.to, serviceRecipe === null || serviceRecipe === void 0 ? void 0 : serviceRecipe.to) : ''));
+        }
+        if (services.length === 0) {
+            return '';
+        }
+        return `services:\n${services.join('\n\n')}`;
+    };
 };
 const render = (recipe, renderer) => {
+    const services = getServiceBuilder(recipe);
     renderer.onFile(async (node) => {
-        if (node.name === 'docker-compose.yml') {
-            const template = await fs_1.promises.readFile(node.fullSourcePath, 'utf8');
-            for (const output of node.getGenerationTargets()) {
-                renderer.renderAsTemplateString(template, { project: recipe.name, services });
+        if (node.name !== 'docker-compose.yml') {
+            if (node.isDir && node.name === 'fstr') {
+                node.skip = true;
             }
+            return;
+        }
+        const template = await fs_1.promises.readFile(node.fullSourcePath, 'utf8');
+        for (const output of node.getGenerationTargets()) {
+            const content = await renderer.renderAsTemplateString(template, { project: recipe.name, services });
+            await fs_1.promises.writeFile(output, content);
+            return [output];
         }
     });
 };
-exports.render = render;
-exports = exports.render;
+module.exports = render;
